1e67071041f489323721b1b81c51939d28e3f587
SangHakLee <leesanghak7298@gmail.com>
2017-04-05 02:32:56 +0900

    realm 생성 완료. 로컬에 데이터 저장 TODO 서버에 저장하기

--- README.md
+++ README.md
@@ -5,3 +5,23 @@
 ## Infra
 
 - Electron
+
+
+## Realm
+realm 쓰기 위해서
+https://github.com/realm/realm-js/issues/765
+```bash
+npm install electron --save-dev
+./node_modules/.bin/electron-rebuild -f -w realm
+```
+
+### Realm macOS 번들 설치
+https://realm.io/kr/docs/get-started/installation/mac/
+
+`start-object-server.command` 오류 발생 시
+```bash
+sudo vi /etc/hosts
+
+# /etc/hosts
+127.0.0.1   localhost
+```
--- main.js
+++ main.js
@@ -2,6 +2,29 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+// realm 쓰기 위해서
+// https://github.com/realm/realm-js/issues/765
+// npm install electron --save-dev
+// ./node_modules/.bin/electron-rebuild -f -w realm
+const Realm = require('realm')
+
+const Alarm = {name: 'Alarm', properties: {
+  id: {type: 'int', optional: false},
+  title: {type: 'string', optional: true}
+}}
+
+const db = new Realm({schema: [Alarm]})
+let alarm = db.objects('Alarm')
+
+db.write( () => {
+  console.log('alarm',alarm.length)
+  // let id = alarm[alarm.length-1].id  || 1
+  let id = alarm[alarm.length-1] ? alarm[alarm.length-1].id : 1
+  db.create('Alarm', {id: id+1, title: 'Test'})
+})
+
+console.log(db.objects('Alarm'))
+
 let win
 
 function createWindow () {
